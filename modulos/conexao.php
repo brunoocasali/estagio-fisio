fdb430488ee4e5b0b5cc60c43c50e3a1b2c13d9d
Erick Sutil <ericksutil@gmail.com>
2015-10-15 02:04:40 -0300

    Update conexao.php

--- modulos/conexao.php
+++ modulos/conexao.php
@@ -2,8 +2,8 @@
 
 header('Content-Type:text/html; charset=utf-8');
 
-$con = @mysql_connect("localhost", "root", "") or die("Não foi possível conectar com o servidor de dados!");
-mysql_select_db("fisio", $con) or die("Banco de dados não localizado!");
+$con = @mysql_connect("mysql2.000webhost.com", "a2671977_root", "5Util2012") or die("Não foi possível conectar com o servidor de dados!");
+mysql_select_db("a2671977_fisio", $con) or die("Banco de dados não localizado!");
     
 mysql_query("SET NAMES 'utf8'");
 mysql_query('SET character_set_connection=utf8');
